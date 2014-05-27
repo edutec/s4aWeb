e3718823424afd4f23db7a39eac0dea6c4bc563b
Bernat Romagosa <scratch@e-citilab.eu>
2014-05-27 09:40:34 +0200

    MacOSX version on the way

--- snap/downloads/newRelease.sh
+++ snap/downloads/newRelease.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cp ~/Scratch/snap/snap4arduino/dev/release/win32/snap4arduino.zip win32/
 cp ~/Scratch/snap/snap4arduino/dev/release/gnu/snap4arduino.tar.gz gnu/
+cp ~/Scratch/snap/snap4arduino/dev/release/mac/snap4arduino.zip mac/
