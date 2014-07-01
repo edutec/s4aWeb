d3cf4961c972cc6ade8573b3d292054028e199df
Bernat Romagosa <scratch@e-citilab.eu>
2014-07-01 12:28:32 +0200

    Snap4Arduino 1.0 alpha launched for gnu and win32

--- snap/downloads/newRelease.sh
+++ snap/downloads/newRelease.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cp ~/Scratch/snap/snap4arduino/dev/release/win32/snap4arduino.zip win32/
-cp ~/Scratch/snap/snap4arduino/dev/release/gnu/snap4arduino.tar.gz gnu/
-cp ~/Scratch/snap/snap4arduino/dev/release/mac/snap4arduino.zip mac/
+cp ~/Scratch/snap/snap4arduino/dev/release/win32/Snap4Arduino.zip win32/
+cp ~/Scratch/snap/snap4arduino/dev/release/gnu/Snap4Arduino.tar.gz gnu/
+cp ~/Scratch/snap/snap4arduino/dev/release/mac/Snap4Arduino.zip mac/
