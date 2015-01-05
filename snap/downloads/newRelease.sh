1f6101f503cefd97239f370189db303ce20413db
Bernat Romagosa <scratch@e-citilab.eu>
2015-01-05 12:43:36 +0100

    64b gnu/linux version published

--- snap/downloads/newRelease.sh
+++ snap/downloads/newRelease.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 cp ~/Scratch/snap/snap4arduino/dev/release/win32/Snap4Arduino.zip win32/
 cp ~/Scratch/snap/snap4arduino/dev/release/gnu/Snap4Arduino.tar.gz gnu/
+cp ~/Scratch/snap/snap4arduino/dev/release/gnu64/Snap4Arduino.tar.gz gnu64/
 cp ~/Scratch/snap/snap4arduino/dev/release/osx/Snap4Arduino.zip mac/
